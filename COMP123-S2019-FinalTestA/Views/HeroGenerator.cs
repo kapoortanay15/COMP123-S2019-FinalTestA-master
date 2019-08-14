6b3e78cf9597dcf4d0fc2f4eb51079a6a67a95a0
Tanay Kapoor <kapoortanay15@gmail.com>
2019-08-14 12:08:16 -0400

    Add project files.

---
+++ COMP123-S2019-FinalTestA/Views/HeroGenerator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+/*
+ * STUDENT NAME: 
+ * STUDENT ID:
+ * DESCRIPTION: This is the Here Generator Form
+ */
+
+namespace COMP123_S2019_FinalTestA.Views
+{
+    public partial class HeroGenerator : COMP123_S2019_FinalTestA.Views.MasterForm
+    {
+        public HeroGenerator()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is the event handler for the BackButton Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            if (MainTabControl.SelectedIndex != 0)
+            {
+                MainTabControl.SelectedIndex--;
+            }
+        }
+
+        /// <summary>
+        /// This is the event handler for the NextButton Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            if(MainTabControl.SelectedIndex < MainTabControl.TabPages.Count - 1)
+            {
+                MainTabControl.SelectedIndex++;
+            }
+        }
+
+        private void GenerateNameButton_Click(object sender, EventArgs e)
+        {
+            GenerateNames();
+        }
+        public void loadnames()
+        {
+            //Inputing Last Name
+            var lastName = File.ReadAllLines("lastNames.txt");
+            var lastNameList = new List<string>(lastName);
+
+            //Inputing First Name
+            var firstName = File.ReadAllLines("firstNames.txt");
+            var firstNameList = new List<string>(firstName);
+        }
+
+       
+
+        public void GenerateNames()
+        {
+            //Inputing First Name
+            var firstName = File.ReadAllLines("firstNames.txt");
+            var firstNameList = new List<string>(firstName);
+            Random rand = new Random();
+            int index = rand.Next(firstNameList.Count);
+            FirstNameDataLabel.Text = firstNameList[index];
+
+            //Inputing Last Name
+            var lastName = File.ReadAllLines("lastNames.txt");
+            var lastNameList = new List<string>(lastName);
+            Random random = new Random();
+            int indexs = random.Next(lastNameList.Count);
+            LastNameDataLabel.Text = lastNameList[indexs];
+        }
+
+        private void HeroGenerator_Load(object sender, EventArgs e)
+        {
+            loadnames();
+            GenerateNames();
+        }
+    }
+
+}
+
